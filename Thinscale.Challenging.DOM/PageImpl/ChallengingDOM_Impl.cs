8b21855438913dafc686f460bc874dc3265539e2
DD <davidsxbl@hotmail.co.uk>
2022-08-30 10:51:10 +0100

    DEMO CIDEO PIPELINE

--- Thinscale.Challenging.DOM/PageImpl/ChallengingDOM_Impl.cs
+++ Thinscale.Challenging.DOM/PageImpl/ChallengingDOM_Impl.cs
@@ -183,7 +183,7 @@ namespace Thinscale.Challenging.DOM.PageImpl
             String expectedValue2 = "Ipsum";
             String expectedValue3 = "Dolor";
             String expectedValue4 = "Sit";
-            String expectedValue5 = "Amet"; 
+            String expectedValue5 = "Ammet"; 
             String expectedValue6 = "Diceret";
             String expectedValue7 = "Action";
 
